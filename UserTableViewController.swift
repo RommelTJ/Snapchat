8bad7f1f0a830e356b4cf124cc83f0fb1fe31f4c
Rommel Rico <rommeltj@gmail.com>
2015-03-18 20:54:01 -0700

    Sending Images

--- UserTableViewController.swift
+++ UserTableViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class UserTableViewController: UITableViewController {
+class UserTableViewController: UITableViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     var userArray: [String] = []
+    var activeRecipient = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,15 @@ class UserTableViewController: UITableViewController {
         for user in users {
             userArray.append(user.username)
         }
+    }
+    
+    func pickImage(sender: AnyObject) {
+        var image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.Camera
+        image.allowsEditing = true
+        NSLog("About to present the image.")
+        self.presentViewController(image, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,6 +61,34 @@ class UserTableViewController: UITableViewController {
         return cell
     }
 
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        //Upload to Parse Code goes here.
+        var imageToSend = PFObject(className:"Image")
+//        imageToSend["image"] = UIImageJPEGRepresentation(image, 0.1111)
+        imageToSend["photo"] = PFFile(name: "Image", data: UIImageJPEGRepresentation(image, 0.5))
+        imageToSend["senderUsername"] = PFUser.currentUser().username
+        imageToSend["recipientUsername"] = userArray[activeRecipient]
+        imageToSend.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError!) -> Void in
+            if (success) {
+                // The object has been saved.
+                NSLog("Image sent succesfully!")
+            } else {
+                // There was a problem, check error.description
+                NSLog("ERROR: \(error.description)")
+            }
+        }
+        
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        activeRecipient = indexPath.row
+        pickImage(self)
+    }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
